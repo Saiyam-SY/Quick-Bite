50e0399a8e3581e42274c90f0f27e36b23491a16
Saiyam Yadav <ydvsaiyam001@gmail.com>
2026-04-29 19:42:38 +0530

    create user mongoose model with authentication and role fields

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "owner", "deliveryBoy"],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
