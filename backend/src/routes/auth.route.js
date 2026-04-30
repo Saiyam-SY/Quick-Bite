56124ca2f732e8922afa5a936548cba0219b7b28
Saiyam Yadav <ydvsaiyam001@gmail.com>
2026-04-30 21:04:24 +0530

    feat(auth): initialize frontend and connect signup page with backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
-router.post("/signout", signOutc);
+router.post("/signout", signOut);
 
 export default router;
